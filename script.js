0b9884919b2caf55ffe230cecfb0601545bf5a6c
Ali <alifeyza4017@gmail.com>
2022-09-18 08:53:13 +0300

    added

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const btnNew = document.querySelector(".btn--new")
 const btnRoll = document.querySelector(".btn--roll")
 const btnHold = document.querySelector(".btn--hold")
 
+const currentScore0 = document.querySelector("#current--0")
+const currentScore1 = document.querySelector("#current--1")
+
 // Starting Conditioans
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.className = "hidden"
 
-
+let currentScore = 0
 // Rolling dice functionality
 btnRoll.addEventListener("click", () => {
     // 1 Generating a random dice roll
@@ -20,5 +23,14 @@ btnRoll.addEventListener("click", () => {
     // 2 Display dice
     diceEl.classList.remove("hidden")
     diceEl.src = `./img/dice-${dice}.png`
+
     // 3 Check for rolled 1 : if true, swtich to next player
+    if(dice !==1) {
+        // Add dice to current score
+        currentScore +=dice
+        currentScore0.textContent = currentScore
+
+    } else {
+        // Swtich player
+    }
 })
